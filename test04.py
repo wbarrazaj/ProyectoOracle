35a538264e9ebeec2ef0857ba6c7c0e7d75d4f01
wbarrazaj <wbarraza@gmail.com>
2022-10-06 15:06:24 -0300

    .

---
+++ test04.py
@@ -0,0 +1,45 @@
+#import sqlanydb
+import os
+import pyodbc2
+import sqlite3
+from clases.cls_Bdd import BaseDD
+
+con = sqlite3.connect("SmartDB.db") 
+
+cur = con.cursor()
+res_conn = cur.execute("select Motor, Servidor, Usuario, Clave, BDD, Puerto from Tbl_Conexion where Servidor = 'Ares';") 
+Motor, Servidor, Usuario, Clave, BDD, Puerto =res_conn.fetchone() 
+print(Motor, Servidor, Usuario, Clave, BDD, Puerto) 
+
+print("")
+
+res_consulta = cur.execute("select Motor, Tipo, Consulta, Fecha from Tbl_Indicadores where Motor='MariaDB';")  
+res_cons_Motor, res_cons_Tipo, res_cons_Consulta, res_cons_Fecha =res_consulta.fetchone()
+
+print(res_cons_Motor, res_cons_Tipo, res_cons_Consulta, res_cons_Fecha)
+
+
+con.close()
+
+
+
+"""
+Conn = BaseDD(servidor,usuario,clave,db,puerto,drver)
+
+
+print(Conn.ServidorDB)
+a=pyodbc.drivers()
+
+print(a)
+print(Conn)
+
+conn = pyodbc.connect(driver=drver, server=servidor , database=db ,uid=usuario , pwd=clave)
+
+print("Aqui")
+
+b=conn.conectar()
+
+print(b)
+print("Termino")
+
+"""
