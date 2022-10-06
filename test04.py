04d4006d8acf3e8773e859b51c7aa48d80f1a42c
wbarrazaj <wbarraza@gmail.com>
2022-10-06 16:58:44 -0300

    .

--- test04.py
+++ test04.py
@@ -16,7 +16,7 @@ print("")
 res_consulta = cur.execute("select Motor, Tipo, Consulta, Fecha from Tbl_Indicadores where Motor='MariaDB';")  
 res_cons_Consulta =res_consulta.fetchall()
 
-print(res_cons_Consulta[0])
+print("Consulta : ", res_cons_Consulta[0])
 
 Servidor='Ares2'
 
