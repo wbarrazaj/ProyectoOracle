fc2d2cb5d8036be8433c93ef8d52a0f537609745
wbarrazaj <wbarraza@gmail.com>
2022-10-06 21:08:10 -0300

    .

--- test04.py
+++ test04.py
@@ -29,14 +29,20 @@ dbConn=BaseDD(servidor=Servidor, usuario=Usuario, clave=Clave, db=BDD, puerto=Pu
 
 print (dbConn.ServidorDB,dbConn.SchemaDBD, dbConn.UsuarioDB)
 
-i=0
 for a in res_cons_Consulta:
     print("Consulta : ", a[0] )
     resultado=dbConn.ejecutar_query(a[3])
     fecha_ejecucion = datetime.datetime.now()
     cur.execute("insert into Tbl_Resultados(Servidor, BDD, Motor, Fecha, id_indicador) values (?, ?, ?, ?, ?)", (Servidor, BDD, Motor, fecha_ejecucion, a[0]))
     print(resultado)
-    i=i+1
+
+res_ejec_consulta = cur.execute("select Servidor, BDD, Motor, Fecha, id_indicador from Tbl_Resultados where Motor='MariaDB';")  
+res_ej_Consulta =res_ejec_consulta.fetchall()
+
+
+for b in res_cons_Consulta: 
+    print(b)
+
 
 #res_cons_Consulta.close() 
 
