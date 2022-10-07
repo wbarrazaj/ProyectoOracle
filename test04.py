1408abf106af57d8dfdd9c116a1ebf7dbd696e44
wbarrazaj <wbarraza@gmail.com>
2022-10-06 21:35:19 -0300

    .

--- test04.py
+++ test04.py
@@ -30,9 +30,9 @@ dbConn=BaseDD(servidor=Servidor, usuario=Usuario, clave=Clave, db=BDD, puerto=Pu
 #print (dbConn.ServidorDB,dbConn.SchemaDBD, dbConn.UsuarioDB)
 
 for a in res_cons_Consulta:
-    print("Consulta : ", a[0] )
+    #print("Consulta : ", a[0] )
     resultado=dbConn.ejecutar_query(a[3])
-    print(resultado)
+    #print(resultado)
     id = a[0] 
     fecha_ejecucion = datetime.datetime.now()
     cur.execute("insert into Tbl_Resultados(Servidor, BDD, Motor, Fecha, id_indicador, Resultados) values (?, ?, ?, ?, ?, ?)", (Servidor, BDD, Motor, fecha_ejecucion, id, str(resultado)))
