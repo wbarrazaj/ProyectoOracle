5fe9500085326a8ba45dc92f6d3af13dfee6b4b3
wbarrazaj <wbarraza@gmail.com>
2022-10-14 23:08:34 -0300

    .

--- test04.py
+++ test04.py
@@ -16,44 +16,53 @@ printlog("Inicio .--" )
 con = sqlite3.connect("SmartDB.db") 
 
 cur = con.cursor()
-res_conn = cur.execute("select Motor, Servidor, Usuario, Clave, BDD, Puerto from Tbl_Conexion where Servidor ('Ares','Apolo';") 
-Motor, Servidor, Usuario, Clave, BDD, Puerto =res_conn.fetchone() 
+res_conn_servidor = cur.execute("select Motor, Servidor, Usuario, Clave, BDD, Puerto from Tbl_Conexion where Servidor ('Ares','Apolo';") 
+res_consulta_servidor=res_conn_servidor.fetchall() 
+
+#Motor, Servidor, Usuario, Clave, BDD, Puerto =
+
+for aaa in res_consulta_servidor :
+    Motor = aaa[0]
+    Servidor = aaa[1]
+    Usuario = aaa[2]
+    Clave = aaa[3]
+    BDD = aaa[4]
+    Puerto = aaa[5]
+
+    res_consulta = cur.execute("select Id, Motor, Tipo, Consulta, Fecha, Tabla, Estructura from Tbl_Indicadores where Motor='MariaDB';")  
+    res_cons_Consulta =res_consulta.fetchall()
+
+    #Servidor='Ares2'
+
+    dbConn=BaseDD(servidor=Servidor, usuario=Usuario, clave=Clave, db=BDD, puerto=Puerto, drver='', motor='MariaDB') 
+
+    Status=Indicadores(Id=0, Motor=dbConn.Motor,conn=con)
+
+    dbConn.ejecutar_query('select 1 ;')
 
 
-res_consulta = cur.execute("select Id, Motor, Tipo, Consulta, Fecha, Tabla, Estructura from Tbl_Indicadores where Motor='MariaDB';")  
-res_cons_Consulta =res_consulta.fetchall()
+    if dbConn.Estado==1:
+        printlog ("Base de Datos Down :  Servidor ---> " + dbConn.ServidorDB + " BDD ---> " + dbConn.SchemaDBD )
+        Status.insert_tbl_Estado_BDD(0, Motor,Servidor,BDD, 'DOWN')
+    else :
+        printlog ("Base de Datos UP :  Servidor ---> " + dbConn.ServidorDB + " BDD ---> " + dbConn.SchemaDBD )
+        Status.insert_tbl_Estado_BDD(0, Motor,Servidor,BDD, 'UP')
+        for a in res_cons_Consulta:
+            resultado=dbConn.ejecutar_query(a[3])
+            id = a[0] 
+            fecha_ejecucion = datetime.datetime.now()
+            printlog ("Tabla--->"+ a[5])
+            Ind_=Indicadores(Id=id, Motor=dbConn.Motor,conn=con)
 
-#Servidor='Ares2'
+            try:
+                for exec_cons in resultado :
+                    Dato = [Ind_.Id, Ind_.Motor,Servidor,BDD,fecha_ejecucion ]
+                    for exec_y in range(Ind_.cant_campos-5):
+                        Dato.append(str(exec_cons[exec_y]))
+                    Ind_.insert_tbl(Dato,Ind_.Id)
 
-dbConn=BaseDD(servidor=Servidor, usuario=Usuario, clave=Clave, db=BDD, puerto=Puerto, drver='', motor='MariaDB') 
-
-Status=Indicadores(Id=0, Motor=dbConn.Motor,conn=con)
-
-dbConn.ejecutar_query('select 1 ;')
-
-
-if dbConn.Estado==1:
-    printlog ("Base de Datos Down :  Servidor ---> " + dbConn.ServidorDB + " BDD ---> " + dbConn.SchemaDBD )
-    Status.insert_tbl_Estado_BDD(0, Motor,Servidor,BDD, 'DOWN')
-else :
-    printlog ("Base de Datos UP :  Servidor ---> " + dbConn.ServidorDB + " BDD ---> " + dbConn.SchemaDBD )
-    Status.insert_tbl_Estado_BDD(0, Motor,Servidor,BDD, 'UP')
-    for a in res_cons_Consulta:
-        resultado=dbConn.ejecutar_query(a[3])
-        id = a[0] 
-        fecha_ejecucion = datetime.datetime.now()
-        printlog ("Tabla--->"+ a[5])
-        Ind_=Indicadores(Id=id, Motor=dbConn.Motor,conn=con)
-
-        try:
-            for exec_cons in resultado :
-                Dato = [Ind_.Id, Ind_.Motor,Servidor,BDD,fecha_ejecucion ]
-                for exec_y in range(Ind_.cant_campos-5):
-                    Dato.append(str(exec_cons[exec_y]))
-                Ind_.insert_tbl(Dato,Ind_.Id)
-
-        except sqlite3.Error as er:
-            printlog (er)
-    con.close()
+            except sqlite3.Error as er:
+                printlog (er)
+con.close()
 
 printlog ("Termino .-- ")
